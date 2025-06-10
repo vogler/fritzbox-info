b3c0bdffd1c5bbebe07ee59bb750c4a6463df35e
Ralf Vogler <ralf.vogler@gmail.com>
2025-06-10 14:56:38 +0200

    cleanup: uncommitted changes from 2024-01-22

--- devices.mjs
+++ devices.mjs
@@ -15,12 +15,13 @@ read from environment variables FBHOST, FBUSER and FBPASS,
 or, if nothing is set, it will prompt you.
 
 Options:
-  --loop=SEC    Run in a loop with SEC seconds of sleep each iteration.
-  --host=FBHOST   FritzBox hostname including port, e.g., foobarbaz.myfritz.net:46390.
-  --user=FBUSER   FritzBox username.
-  --pass=FBPASS   FritzBox password.
-  --verbose     Verbose mode (shows each command and its output; off by default).
-  --help        Show this usage information.
+  --host=FBHOST  FritzBox hostname including port, e.g., foobarbaz.myfritz.net:46390.
+  --user=FBUSER  FritzBox username.
+  --pass=FBPASS  FritzBox password.
+  --loop=SEC     Run in a loop with SEC seconds of sleep each iteration.
+  --add_mac=MAC  Add a device by its MAC address and quit.
+  --verbose      Verbose mode (shows each command and its output; off by default).
+  --help         Show this usage information.
 `);
 }
 if (argv.help) {
@@ -62,65 +63,78 @@ const host = argv.host || process.env.FBHOST || await question('Hostname includi
 const user = argv.user || process.env.FBUSER || await question('Username: ') || process.exit(1)
 const pass = argv.pass || process.env.FBPASS || await ask('Password: ', true) || process.exit(1)
 
-// await spinner('working...', () => $`sleep 2`)
+const headers = {
+  "accept": "*/*",
+  "accept-language": "en-US,en;q=0.9,de-DE;q=0.8,de;q=0.7",
+  "cache-control": "no-cache",
+  "content-type": "application/x-www-form-urlencoded",
+  "pragma": "no-cache",
+  "sec-ch-ua": "\"Chromium\";v=\"116\", \"Not)A;Brand\";v=\"24\", \"Google Chrome\";v=\"116\"",
+  "sec-ch-ua-mobile": "?0",
+  "sec-ch-ua-platform": "\"macOS\"",
+  "sec-fetch-dest": "empty",
+  "sec-fetch-mode": "cors",
+  "sec-fetch-site": "same-origin",
+};
+let sid = null;
+const getData = (host) => async (page, json = true) => {
+  // check if sid still valid and login+challenge if not
+  // http://www.apfel-z.net/artikel/Fritz_Box_API_via_curl_wget/
+  if (!sid) {
+    // console.log('> Login <')
+    // console.time('login');
+    const sid_get = await (await fetch(`https://${host}/login_sid.lua`, { headers, "method": "GET" })).text();
+    // console.log(sid_get);
+    const xml_sid = t => t.match(/<SID>(.*)<\/SID>/)[1];
+    const xml_cha = t => t.match(/<Challenge>(.*)<\/Challenge>/)[1];
+    if (xml_sid(sid_get) == '0000000000000000') {
+      const challenge = xml_cha(sid_get);
+      // console.error('Not logged in anymore! Challenge:', challenge);
+      const response = (await $`echo -n "${challenge}-${pass}" | iconv --from-code=UTF-8 --to-code=UTF-16LE | md5sum | sed  -e 's/ .*//'`).toString().trim();
+      // console.log('Response:', response);
+      const r = await (await fetch(`https://${host}/login_sid.lua`, { headers, "method": "POST",
+        "body": `response=${challenge}-${response}&username=${user}`,
+      })).text();
+      // console.log(r);
+      sid = xml_sid(r);
+      // console.log('New SID:', sid);
+    }
+    // console.timeEnd('login');
+  }
+  console.time(page);
+  const r = await fetch(`https://${host}/data.lua`, {
+    headers,
+    "body": `xhr=1&sid=${sid}&page=${page}&lang=de&xhrId=all&initial=`,
+    "method": "POST"
+  });
+  console.timeEnd(page);
+  return await (json ? r.json() : r.text());
+};
+
+const fb = getData(host);
+
+if (argv.add_mac) {
+  const mac = argv.add_mac;
+  console.log('Adding device with MAC', mac);
+  const formData = mac.split(':').map((i,v) => `mac${i}=${v}`).join('&') + `&mac=${encodeURIComponent(mac)}`;
+  const res = fb(`wKey&${formData}`).data;
+  if (res?.add_mac == 'ok') {
+    console.log('Success!');
+    system.exit(0);
+  } else {
+    console.error('Failed!');
+    system.exit(1);
+  }
+}
 
 while(true) {
   console.log(new Date().toLocaleString('de'));
-
-  const headers = {
-    "accept": "*/*",
-    "accept-language": "en-US,en;q=0.9,de-DE;q=0.8,de;q=0.7",
-    "cache-control": "no-cache",
-    "content-type": "application/x-www-form-urlencoded",
-    "pragma": "no-cache",
-    "sec-ch-ua": "\"Chromium\";v=\"116\", \"Not)A;Brand\";v=\"24\", \"Google Chrome\";v=\"116\"",
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "\"macOS\"",
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "same-origin",
-  };
-  let sid = null;
-  const getData = (fb) => async (page, json = true) => {
-    // check if sid still valid and login+challenge if not
-    // http://www.apfel-z.net/artikel/Fritz_Box_API_via_curl_wget/
-    if (!sid) {
-      // console.log('> Login <')
-      // console.time('login');
-      const sid_get = await (await fetch(`https://${fb}/login_sid.lua`, { headers, "method": "GET" })).text();
-      // console.log(sid_get);
-      const xml_sid = t => t.match(/<SID>(.*)<\/SID>/)[1];
-      const xml_cha = t => t.match(/<Challenge>(.*)<\/Challenge>/)[1];
-      if (xml_sid(sid_get) == '0000000000000000') {
-        const challenge = xml_cha(sid_get);
-        // console.error('Not logged in anymore! Challenge:', challenge);
-        const response = (await $`echo -n "${challenge}-${pass}" | iconv --from-code=UTF-8 --to-code=UTF-16LE | md5sum | sed  -e 's/ .*//'`).toString().trim();
-        // console.log('Response:', response);
-        const r = await (await fetch(`https://${fb}/login_sid.lua`, { headers, "method": "POST",
-          "body": `response=${challenge}-${response}&username=${user}`,
-        })).text();
-        // console.log(r);
-        sid = xml_sid(r);
-        // console.log('New SID:', sid);
-      }
-      // console.timeEnd('login');
-    }
-    console.time(page);
-    const r = await fetch(`https://${fb}/data.lua`, {
-      headers,
-      "body": `xhr=1&sid=${sid}&page=${page}&lang=de&xhrId=all&initial=`,
-      "method": "POST"
-    });
-    console.timeEnd(page);
-    return await (json ? r.json() : r.text());
-  };
-
-  const fb = getData(host);
-
   // console.log('> Devices <')
   console.log((await fb('overview')).data.net.devices.map(x => `${x.name} (${x.desc})`)); // 3s
+  // data.dsl.{down,up}
   // console.log((await fb('netDev')).data.active.map(x => x.name)); // 5s
   // icons: green = connected, globe = connected and using internet sending/receiving data; https://www.gutefrage.net/frage/was-bedeuten-fritzbox-icons
+  // echo "$(echo 'name,mac,ipv4.ip,port'; cat netDev.json | jq -r '.data | ([.active, .passive] | add)[] | [.name, .mac, .ipv4.ip, .port] | @csv')" | xsv table
 
   // console.log('> Online-Zähler <')
   const netCnt = await fb('netCnt', false); // 0.5s
